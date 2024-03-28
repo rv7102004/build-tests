832c2d778c4354a1cd766845491001c6b39ac492
gemtechd <gemtech.d@gmail.com>
2024-03-28 14:56:01 +0200

    calculate distance function

---
+++ modules/geometry-calculation.js
@@ -0,0 +1,13 @@
+const calculateDistance = (point1, point2) => {
+    let distanceX = point2.x - point1.x ** 2;
+    let distanceY = point2.y - point2.y ** 2;
+    const distance = Math.sqrt(distanceX + distanceY);
+    return distance;
+}
+
+const calculateJunctionPoint = (lin1, line2)=>{
+    
+}
+const calculateAngle = (line1, line2) =>{
+
+}
