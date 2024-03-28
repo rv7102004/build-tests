d6c1c6f57045afb8863146e5d5ff9df8fda1430b
gemtechd <gemtech.d@gmail.com>
2024-03-28 15:31:26 +0200

    build the line class

---
+++ modules/ecs6-class/line.js
@@ -0,0 +1,11 @@
+class Line{
+    constructor({point1, point2, incline}){
+        this.point1 = point1;
+        this.point2  = point2;
+        this.incline = incline;
+    }
+
+    isPointOnLine(point){
+
+    }
+}
