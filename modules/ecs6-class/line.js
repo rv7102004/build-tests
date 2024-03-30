3010ff06360dae3598bdba4602c184fed4346436
gemtechd <gemtech.d@gmail.com>
2024-03-31 01:35:56 +0300

    changes in line class

--- modules/ecs6-class/line.js
+++ modules/ecs6-class/line.js
@@ -1,8 +1,8 @@
 class Line{
-    constructor({point1, point2, incline}){
+    constructor({point1, point2, slope = 0}){
         this.point1 = point1;
         this.point2  = point2;
-        this.incline = incline;
+        this.slope = slope;
     }
 
     isPointOnLine(point){
