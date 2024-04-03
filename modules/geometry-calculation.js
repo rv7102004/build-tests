a6697d6b0a47cc90e0fcdcc5eebef7c16a0ff3a5
gemtechd <gemtech.d@gmail.com>
2024-04-04 01:56:57 +0300

    fixed calculateDistance function

--- modules/geometry-calculation.js
+++ modules/geometry-calculation.js
@@ -1,6 +1,6 @@
 const calculateDistance = (point1, point2) => {
-    let distanceX = point2.x - point1.x ** 2;
-    let distanceY = point2.y - point2.y ** 2;
+    let distanceX = (point2.x - point1.x) ** 2;
+    let distanceY = (point2.y - point2.y) ** 2;
     const distance = Math.sqrt(distanceX + distanceY);
     return distance;
 }
