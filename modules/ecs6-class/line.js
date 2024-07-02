afb098933f5d218bc1f40abcafeb5a662fb5f165
gemtechd <gemtech.d@gmail.com>
2024-07-03 00:46:50 +0300

    changes

--- modules/ecs6-class/line.js
+++ modules/ecs6-class/line.js
@@ -1,89 +1,38 @@
-const { calculateSlope, calculateNOfLineFunction } = require("../geometry-calculation");
 const Point = require("./point");
 
 class Line {
-    constructor({ point1 = undefined, point2 = undefined, n = undefined, slope = undefined }) {
+    constructor({ point1 = new Point(), point2 = new Point(), n = undefined, slope = undefined }) {
         this.point1 = point1;
         this.point2 = point2;
         this.slope = slope;
         this.n = n;
     }
-    addPoint(point) {
-        if (this.point1 === undefined) {
-            this.point1 = point
-        }
-        else {
-            if (this.point2 === undefined) {
-                this.point2 = point
-            }
-        }
+
+    calculateSlope = () => {
+        this.slope = (this.point1.y - this.point2.y) / (this.point1.x - this.point2.x)
     }
 
-    get Points() {
-        return { point1: this.point1, point2: this.point2 }
-    }
-
-    set Slope(slope) {
-        this.slope = slope;
-    }
-
-    get Slope() {
-        if (!this.slope) {
-            if (this.point1 && this.point2) {
-                this.slope = calculateSlope(this.point1, this.point2)
-            }
-        }
-        return this.slope
-    }
-
-    set N(n) {
-        this.n = n;
-    }
-
-    get N() {
-        if (!this.n) {
-            if ((this.point1 || this.point2) && this.slope)
-                this.n = calculateNOfLineFunction({ point: this.point1 | this.point2, slope: this.slope })
-        }
-        return this.n
+    calculateNOfLineFunction = () => {
+        this.n = this.point1.y - this.slope * this.point1.x
     }
 
     getPointOnXAsis() {
-        if (this.slope) {
-            return this.getPointByX(0)
-        }
-        return undefined
+        return this.getPointByY(0)
     }
 
     getPointOnYAsis() {
-        if (this.slope) {
-            return this.getPointByY(0)
-        }
-        return undefined
-    }
-    isPointOnLine(point) {
-        const slope2 = calculateSlope(this.point1 | this.point2, point)
-        if (this.slope === slope2) {
-            const n2 = calculateNOfLineFunction({ point, slope: slope2 })
-            if (this.n === n2) {
-                return true
-            }
-        }
-        return false
+        return this.getPointByX(0)
     }
 
+
     getPointByX(x) {
-        if (this.slope && this.n) {
-            let y = this.slope * x + this.n
-            return new Point({ x, y })
-        }
+        let y = this.slope * x + this.n
+        return new Point({ x, y })
     }
 
     getPointByY(y) {
-        if (this.slope && this.n) {
-            let x = (y - this.slope) / this.n;
-            return new Point({ x, y })
-        }
+        let x = (y - this.slope) / this.n;
+        return new Point({ x, y })
     }
 }
 
--- modules/geometry-calculation.js
+++ modules/geometry-calculation.js
@@ -1,3 +1,5 @@
+const Line = require('./ecs6-class/line')
+
 const calculateDistance = (point1, point2) => {
     let distanceX = (point2.x - point1.x) ** 2;
     let distanceY = (point2.y - point2.y) ** 2;
@@ -16,30 +18,25 @@ const calculateJunctionPoint = (line1, line2) => {
     }
     else {
         const x = (line1.n - line2.n) / (line2.slope - line1.slope)
-        console.log({x})
-        console.log(line2.getPointByX(x))
-        return line1.getPointByX(x)
+        const junctionPoint = line1.getPointByX(x);
+        return junctionPoint
     }
 }
 
-const calculateAngle = (line1, line2) => {
-
-}
-
-const calculateNOfLineFunction = ({ point, slope }) => {
-    const n = point.y - slope * point.x
-    return n;
-}
-
-const calculateSlope = (point1, point2) => {
-    const slope = (point1.y - point2.y) / (point1.x - point2.x)
-    return slope
+const isPointOnLine = (line, point) => {
+    const proxyLine = new Line(line.point1, point)
+    proxyLine.calculateSlope()
+    if (line.slope === proxyLine.slope) {
+        proxyLine.calculateNOfLineFunction()
+        if (line.n === proxyLine.n2) {
+            return true
+        }
+    }
+    return false
 }
 
 module.exports = {
     calculateDistance,
-    calculateAngle,
-    calculateSlope,
-    calculateNOfLineFunction,
-    calculateJunctionPoint
+    calculateJunctionPoint,
+    isPointOnLine
 }
